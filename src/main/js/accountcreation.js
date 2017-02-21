809e62d7a91b3be9c332900232cb82df91de863b
Ethan Jackson <jackson.ethan.c@gmail.com>
2017-02-21 09:48:40 -0500

    Update accountcreation.js

---
+++ src/main/js/accountcreation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class AccountCreation extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div>
+                Account creation elements go here.
+            </div>
+        );
+    }
+}

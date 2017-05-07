b613f9c79e5a87370c89eda188b001fe3450d1c9
vsubasri <vsubasri@uwo.ca>
2017-05-07 18:59:11 -0400

    Update accountcreation.js

--- src/main/js/accountcreation.js
+++ src/main/js/accountcreation.js
@@ -1,15 +1,60 @@
 import React from 'react';
 
-export class AccountCreation extends React.Component {
+var Account = React.createClass({
 
-    constructor() {
-        super();
+    getInitialState () {
+        return {
+            UserName : "",
+            Message : "",
+        }
+    },
+
+    handleAccountNameChange (e) {
+        e.preventDefault();
+            this.setState({ UserName : e.target.value , Message : "..."
+        });
+    },
+
+    handleSubmit(e) {
+        // Prevents reinitialization
+        e.preventDefault();
+        let name = this.state.UserName;
+        fetch('http://localhost:8080/accountCreation/createAccount?'+ 'userName=' + name, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res =>{
+            if(res.ok){
+                this.setState({Message:'Account created!'});
+            } else{
+                this.setState({Message:  name + ' is already taken.'});
+            }
+        })
+    },
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input type="text" defaultValue={this.state.UserName} onChange={this.handleAccountNameChange}/>
+                    </label>
+                    <input type="submit" value="Create Account!" />
+                </form>
+            UserName: {this.state.UserName}
+            <br/>
+            Message: {this.state.Message}
+            </div>
+        );
     }
+});
 
-    render() {
+export class AccountCreation extends React.Component{
+    render(){
         return(
             <div>
-                Account creation elements go here.
+                <Account/>
             </div>
         );
     }
